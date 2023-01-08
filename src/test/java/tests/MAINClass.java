7416ac63a355e200d9a60191be50b0619c00eff7
Hasan tasar <hasantasar@hotmail.com>
2023-01-08 16:52:56 +0300

    görev tanımları yüklendi

--- src/test/java/tests/MAINClass.java
+++ src/test/java/tests/MAINClass.java
@@ -1,7 +1,20 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+import java.util.List;
+
 public class MAINClass {
-    public static void main(String[] args) {
-        System.out.println("Ortak Class Açıldı");
-    }
+
+    //trendyol daki en yüksek fiyatlı nutella; ege
+    //amazon.tr daki en yüksek fiyatlı nutella; safa
+    //hepsiburada daki en yüksek fiyatlı nutella; hakan
+    //n11'deki daki en yüksek fiyatlı nutella; yaşar
+
+
+
 }
