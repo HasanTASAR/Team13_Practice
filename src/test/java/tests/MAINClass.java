8c0305362159f8f33f538b8dcb55a66a5254f0d5
Hasan tasar <hasantasar@hotmail.com>
2023-01-08 17:39:32 +0300

    yeni görevler eklendi

--- src/test/java/tests/MAINClass.java
+++ src/test/java/tests/MAINClass.java
@@ -4,4 +4,15 @@ public class MAINClass {
     public static void main(String[] args) {
         System.out.println("Ortak Class Açıldı");
     }
+
+    //trendyol daki en yüksek fiyatlı nutella; ege
+    //amazon.tr daki en yüksek fiyatlı nutella; safa
+    //hepsiburada daki en yüksek fiyatlı nutella; hakan
+    //n11'deki daki en yüksek fiyatlı nutella; yaşar
+    //çiçek sepeti daki en yüksek fiyatlı nutella; oğuz
+    //trendyol daki en yüksek fiyatlı tobleron; sevgi
+    //amazon.tr daki en yüksek fiyatlı tobleron; duygu
+    //hepsiburada daki en yüksek fiyatlı tobleron; ceyda
+    //n11'deki daki en yüksek fiyatlı tobleron; buşra
+    //çiçek sepeti daki en yüksek fiyatlı tobleron; seda
 }
