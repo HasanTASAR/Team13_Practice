1014ef6ce2909c74d6755622432e2b8637a6d4ab
Hasan tasar <hasantasar@hotmail.com>
2023-01-08 18:39:36 +0300

    Yeni Güncelleme

--- src/test/java/tests/MAINClass.java
+++ src/test/java/tests/MAINClass.java
@@ -1,18 +1,28 @@
 package tests;
 
 public class MAINClass {
-    public static void main(String[] args) {
-        System.out.println("Ortak Class Açıldı");
-    }
 
-    //trendyol daki en yüksek fiyatlı nutella; ege
-    //amazon.tr daki en yüksek fiyatlı nutella; safa
-    //hepsiburada daki en yüksek fiyatlı nutella; hakan
-    //n11'deki daki en yüksek fiyatlı nutella; yaşar
-    //çiçek sepeti daki en yüksek fiyatlı nutella; oğuz
-    //trendyol daki en yüksek fiyatlı tobleron; sevgi
-    //amazon.tr daki en yüksek fiyatlı tobleron; duygu
-    //hepsiburada daki en yüksek fiyatlı tobleron; ceyda
-    //n11'deki daki en yüksek fiyatlı tobleron; buşra
-    //çiçek sepeti daki en yüksek fiyatlı tobleron; seda
+    /*
+    Arkadaşlar birer class açıp orada test kodlarımzı yaza biliriz,
+    ancak kodu tamamlamayı beklemeyiniz.
+    Şimdilik önemli olan bir class açıp kendi branch ınızdan gite puss etmek.
+    Ayrıca yine ortak MAINClasa'a da şimdilik tahmini bir fiyat ekleyebilirsiz.
+    Kendi claslarınzı puslarken kendi branchınızdan ve sadece kendi clasınızı seçerek yapacaksınız
+    ---git add kendiklasımınPath'i
+    Mainclass içindeki ek için ise yine sadece maincllassı seçelek yapalım.
+    git add mainclassPathı     (git add . komutu sorun çıkarabilir bilemiyorum deneye deneye bulacağız)
+
+     */
+
+    //trendyol daki en yüksek fiyatlı nutella; ege = 755 Tl
+    //amazon.tr daki en yüksek fiyatlı nutella; safa = ?
+    //hepsiburada daki en yüksek fiyatlı nutella; hakan = ?
+    //n11'deki daki en yüksek fiyatlı nutella; yaşar = ?
+    //çiçek sepeti daki en yüksek fiyatlı nutella; oğuz = ?
+    //trendyol daki en yüksek fiyatlı tobleron; sevgi= ?
+    //amazon.tr daki en yüksek fiyatlı tobleron; duygu = ?
+    //hepsiburada daki en yüksek fiyatlı tobleron; ceyda = ?
+    //n11'deki daki en yüksek fiyatlı tobleron; buşra = ?
+    //çiçek sepeti daki en yüksek fiyatlı tobleron; seda = ?
+
 }
