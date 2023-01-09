8c2400b6b6fb8b7e17f6f8ee3113963f8e32be92
sedaakca <sakca35@gmail.com>
2023-01-09 14:04:38 +0300

    09.01.2023 Seda

---
+++ src/test/java/tests/Seda.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Seda {
+
+        @Test
+       public void test01(){
+            System.out.println("hello gitup");
+        }
+
+        }
+
+
+
+
+
