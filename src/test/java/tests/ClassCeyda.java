50f8571b98fd7233a91b0e40c3d178dae2d56172
ceydakizik <ceydaozcann@gmail.com>
2023-01-09 13:37:20 +0300

    09.01.20233 Ceyda

---
+++ src/test/java/tests/ClassCeyda.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ClassCeyda {
+
+    @Test
+    public void testCeyda(){
+        System.out.println("Hello World");
+
+    }
+}
