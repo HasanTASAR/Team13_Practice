06cd931a8c602f4f651918dc731e4ed42cd3747d
ipeks <ipeksenci@gmail.com>
2023-01-09 20:10:13 +0300

    ipek

---
+++ src/test/java/tests/Classipek.java
@@ -0,0 +1,10 @@
+package tests;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class Classipek {
+
+    public static void main(String[] args) {
+        System.out.println("deneme1");
+    }
+}
